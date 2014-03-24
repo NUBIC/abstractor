dc94399cdec6cf44cde539fac73990bc841e850b
Michael Gurley <m-gurley@northwestern.edu>
2014-03-24 10:11:25 -0500

    Add 'non-' entry to NegationDetection::manual_negated_match_value? to handle not matching on tokens like 'non-small cell carcinoma' and 'small cell carcinoma'.

--- lib/abstractor/negation_detection.rb
+++ lib/abstractor/negation_detection.rb
@@ -21,7 +21,7 @@ module Abstractor
 
     def self.manual_negated_match_value?(sentence, match_value)
       negated = false
-      ['\bthere is no evidence of a ', '\binsufficient to make the diagnosis of ', '\binsufficient for the diagnosis of ', '\brule out ', '\bnegative for ', '\bno ', '\bnot ', '\bno evidence of ', '\bno evidence of a ', '\brules out the possibility of a ', '\bto exclude the possibility of '].each do |negation_cue|
+      ['non-','\bthere is no evidence of a ', '\binsufficient to make the diagnosis of ', '\binsufficient for the diagnosis of ', '\brule out ', '\bnegative for ', '\bno ', '\bnot ', '\bno evidence of ', '\bno evidence of a ', '\brules out the possibility of a ', '\bto exclude the possibility of '].each do |negation_cue|
         if !(sentence.downcase.scan(Regexp.new(negation_cue + match_value.downcase)).empty?)
           negated = true
         end
